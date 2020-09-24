beef73e3aac1e336a4a8e308cec52ef79a27fb54
Summerdise <350464517@qq.com>
2020-09-25 01:02:21 +0800

    [zmc] fix: fix cannot back to home page bug

--- src/exercise-1/components/about.js
+++ src/exercise-1/components/about.js
@@ -1,23 +1,17 @@
 import React, { Fragment } from 'react';
-import { BrowserRouter as Router, Route, Switch, Link, BrowserRouter } from 'react-router-dom';
-import Home from "./home";
+import { Link } from 'react-router-dom';
 
 const About = (props) => {
     return (
-        <Router>
-            <Fragment>
-                <p>Company:ThoughtWorks Local</p>
-                <p>Location:Xi`an</p>
-                <br />
-                <p>For more information, please</p>
-                <p>view our
+        <Fragment>
+            <p>Company:ThoughtWorks Local</p>
+            <p>Location:Xi`an</p>
+            <br />
+            <p>For more information, please</p>
+            <p>view our
                 <Link to="/" className="nav_link">website</Link>
-                    <Switch>
-                        <Route exact path="/" component={Home} />
-                    </Switch>
-                </p>
-            </Fragment>
-        </Router>
+            </p>
+        </Fragment>
     )
 }
 
